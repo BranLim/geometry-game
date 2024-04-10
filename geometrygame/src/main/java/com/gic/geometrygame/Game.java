45ec92932502e4a8c8d0f4e549a234e6572505a9
Brandon Lim <limws.brandon@gmail.com>
2024-04-10 11:23:39 +0800

    Add finalisation step.

--- geometrygame/src/main/java/com/gic/geometrygame/Game.java
+++ geometrygame/src/main/java/com/gic/geometrygame/Game.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Game {
 
     private List<Coordinate> coordinates = new ArrayList<>();
+    private boolean finalised;
 
 
     public void addCoordinate(int x, int y) {
@@ -13,19 +14,30 @@ public class Game {
             throw new InvalidArgumentException("Coordinate value cannot be negative");
         }
         Coordinate newCoordinate = new Coordinate(x, y);
-        if (coordinates.stream().anyMatch(coord -> coord.equals(newCoordinate))){
+        if (coordinates.stream().anyMatch(coord -> coord.equals(newCoordinate))) {
             throw new InvalidArgumentException(String.format("New coordinates(%d,%d) is invalid!!!", x, y));
+        }
+        if (coordinates.size() > 1) {
+            Coordinate prev = coordinates.get(coordinates.size() - 1);
+            if (prev.getX() != x && prev.getY() != y) {
+                throw new InvalidArgumentException(String.format("New coordinates(%d,%d) is invalid!!!", x, y));
+            }
         }
         coordinates.add(newCoordinate);
     }
 
     public Coordinate getCoordinate(int coordinateNumber) {
-
         return coordinates.get(coordinateNumber - 1);
     }
 
-    public boolean isShapeCompleted() {
-        return false;
+    public boolean isShapeFinalised() {
+        return this.finalised;
     }
 
+    public void finaliseShape() {
+        if (this.coordinates.size() < 3){
+            throw new IllegalStateException("Shape is incomplete");
+        }
+        this.finalised = true;
+    }
 }
--- geometrygame/src/test/java/com/gic/geometrygame/GameTest.java
+++ geometrygame/src/test/java/com/gic/geometrygame/GameTest.java
@@ -1,15 +1,21 @@
 package com.gic.geometrygame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+    private Game newGame;
+
+    @BeforeEach
+    void setUp() {
+        newGame = new Game();
+    }
 
     @Test
     void shouldBeSuccessfulWhenGiven3ValidCoordinates() {
-        Game newGame = new Game();
         newGame.addCoordinate(1, 1);
         newGame.addCoordinate(5, 1);
         newGame.addCoordinate(5, 5);
@@ -20,26 +26,47 @@ class GameTest {
     }
 
     @Test
-    void shouldReturnIncompleteShapeWhenOnlyOneCoordinateAdded() {
-        Game newGame = new Game();
+    void shouldReturnIncompleteShapeWhenOnlyTwoCoordinatesAdded() {
         newGame.addCoordinate(1, 1);
+        newGame.addCoordinate(5, 1);
 
         assertTrue(newGame.getCoordinate(1).equals(new Coordinate(1, 1)));
-        assertFalse(newGame.isShapeCompleted());
+        assertTrue(newGame.getCoordinate(2).equals(new Coordinate(5, 1)));
+        assertFalse(newGame.isShapeFinalised());
     }
 
     @Test
     void shouldThrowErrorWhenCoordinateIsNegative() {
-        Game newGame = new Game();
         assertThrows(InvalidArgumentException.class, () -> newGame.addCoordinate(-1, 1));
     }
 
-
     @Test
     void shouldThrowErrorWhenCoordinateGivenRepeats() {
-        Game newGame = new Game();
         newGame.addCoordinate(4,2);
         newGame.addCoordinate(2,2);
+
         assertThrows(InvalidArgumentException.class, ()->newGame.addCoordinate(2,2));
     }
+
+    @Test
+    void shouldThrowErrorWhenCoordinatesDoNotConnect() {
+        newGame.addCoordinate(4,2);
+        newGame.addCoordinate(2,2);
+
+        assertThrows(InvalidArgumentException.class, ()->newGame.addCoordinate(5,3));
+    }
+
+    @Test
+    void shouldReturnCompleteShapeWhenAddedAtLeastThreeCoordinatesAndGameIsFinalised() {
+        newGame.addCoordinate(1, 1);
+        newGame.addCoordinate(5, 1);
+        newGame.addCoordinate(5, 5);
+        newGame.finaliseShape();
+
+        assertTrue(newGame.getCoordinate(1).equals(new Coordinate(1, 1)));
+        assertTrue(newGame.getCoordinate(2).equals(new Coordinate(5, 1)));
+        assertTrue(newGame.getCoordinate(3).equals(new Coordinate(5, 5)));
+        assertTrue(newGame.isShapeFinalised());
+    }
+
 }
