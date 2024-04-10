e37e43d48245fe86d6522367608397486ee607d5
Brandon Lim <limws.brandon@gmail.com>
2024-04-10 23:44:18 +0800

    Add player

--- geometrygame/src/main/java/com/gic/geometrygame/Player.java
+++ geometrygame/src/main/java/com/gic/geometrygame/Player.java
@@ -1,12 +1,19 @@
 package com.gic.geometrygame;
 
+import java.util.Scanner;
+
 public class Player {
 
-    public Player() {
-
-    }
+    private Scanner scanner = new Scanner(System.in);
 
     public void play(Game game) {
+        if (game==null){
+            throw new IllegalStateException("Game is not ready");
+        }
+        boolean quit = false;
+        while (!quit){
+
+        }
 
     }
 }
